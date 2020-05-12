2d80347a6c30b717b01db6c985a8b975093157da
RafaelRaatz <mrrafa.raatz@gmail.com>
2020-05-12 17:04:53 -0300

    new update

--- src/components/largeNews/index.js
+++ src/components/largeNews/index.js
@@ -10,11 +10,12 @@ const FirstNews = ({ Img, Label, Title, Description }) => {
     return (
 
         <article>
-            <div className="news-thumb">
+            <div className="thumb">
                 <img src={Img} alt="" />
                 <div className="overlay"></div>
             </div>
-            <div className="news-description">
+
+            <div className="largeNewsDescription">
                 <span className="label">{Label}</span>
                 <p className="title">{Title}</p>
                 <p className="description">{Description}</p>
